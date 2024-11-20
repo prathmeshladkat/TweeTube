7cdff9d62e31b72925d0a5ce757d23f34f2eba86
prathmesh ladkat <prathameshladkat336@gmail.com>
2024-11-20 15:31:28 +0530

    commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+//cors document exploration
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+//limit of json daa
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+//router declaration
+app.use("/api/v1/users", userRouter);
+
+export { app };
